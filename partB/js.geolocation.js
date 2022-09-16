0e9323f69d9d9fa31e2b3d8b758716f8a4d248e5
unknown <mullerg@post.bgu.ac.il>
2022-09-16 20:19:54 +0300

    PartB

--- partB/js.geolocation.js
+++ partB/js.geolocation.js
@@ -1,6 +1,9 @@
-/*const courts= require("./DataCourts") */
+
 let Date_Picker;
 let map;
+var service;
+var infowindow;
+
 const x = document.getElementById("geo");
 function getLocation() {
     if (navigator.geolocation) {
@@ -13,8 +16,8 @@ function getLocation() {
   
   function showPosition(position) {
     let Myposition = {lat: position.coords.latitude, lng: position.coords.longitude};
-    x.innerHTML="Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
+    // x.innerHTML="Latitude: " + position.coords.latitude + 
+    // "<br>Longitude: " + position.coords.longitude;
     map.setCenter(Myposition);
    
     const marker = new google.maps.Marker({
@@ -23,7 +26,7 @@ function getLocation() {
       });
       courts.forEach(element => {
       const markerForCourts=new google.maps.Marker({
-
+        
         position: element.coordinates,
         map: map 
         
@@ -65,7 +68,7 @@ function initMap() {
         zoom: 12,
         center: uluru,
       });
-      
+      infowindow = new google.maps.InfoWindow();
   
     }
 
@@ -79,6 +82,56 @@ elem.addEventListener("changeDate",(Event) =>{
   console.log(Event);
 })
 
+const request = {
+  query: "Basketball courts",
+  fields: ["name", "geometry"],
+};
+service = new google.maps.places.PlacesService(map);
 
+service.nearbySearch(
+  request,callback)
+  
+  
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        element(results[i]);
+      }
+    }
+  }
 
+//   var map;
+// var service;
+// var infowindow;
+// const x = document.getElementById("geo");
+// function getLocation() {
+//     if (navigator.geolocation) {
+//       navigator.geolocation.watchPosition(showPosition);
+//     } else { 
+//       x.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+//   } 
+// function initialize() {
+//   var pyrmont = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);  
+//   map = new google.maps.Map(document.getElementById('map'), {
+//       center: pyrmont,
+//       zoom: 15
+//     });
 
+//   var request = {
+//     location: pyrmont,
+//     radius: '500',
+//     type: ['restaurant']
+//   };
+
+//   service = new google.maps.places.PlacesService(map);
+//   service.nearbySearch(request, callback);
+// }
+
+// function callback(results, status) {
+//   if (status == google.maps.places.PlacesServiceStatus.OK) {
+//     for (var i = 0; i < results.length; i++) {
+//       createMarker(results[i]);
+//     }
+//   }
+// }
