b84d232336af1931768a435da0c1635b3490d767
unknown <mullerg@post.bgu.ac.il>
2022-09-17 19:10:01 +0300

    PartBupdated

--- partB/DataCourts.js
+++
@@ -1,1 +0,0 @@
-let courts=[{coordinates:{lat:32.082691,lng:34.777873} ,Name:"court1" },{coordinates:{lat:32.085182,lng:34.777240} ,Name:"court2" }]
--- partB/ServerRequest.js
+++
@@ -1,8 +0,0 @@
-
-function getAvailability(name,date){
-
-    return  {capacity:10,evening:2,morning:5};
-
-
-
-}
